126836d86c5b986f7827dafcee8ec0a1d6c633dc
Angel David Melendez A <63766459+pepoangel@users.noreply.github.com>
2023-03-01 16:10:47 -0400

    Create aritmetica.py

---
+++ apendice/aritmetica.py
@@ -0,0 +1,15 @@
+# aritmetica.py
+
+# Define las funciones para realizar operaciones aritméticas
+
+def sumar(x, y):
+    return x + y   # suma los valores de x e y
+
+def dividir(x, y):
+    return x / y   # divide el valor de x entre el valor de y
+
+def multiplicar(x, y):
+    return x * y   # multiplica los valores de x e y
+
+def restar(x, y):
+    return x - y   # resta el valor de y al valor de x
